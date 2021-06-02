0b28e88234d6f5cd0ce07a6fe46394af81be1da1
SindhujaCheemala <sindhujacheemala@gmail.com>
2021-06-02 11:34:30 +0530

    first commit

--- src/main/java/com/hcl/employeedirectory/EmployeeDirectoryApplication.java
+++ src/main/java/com/hcl/employeedirectory/EmployeeDirectoryApplication.java
@@ -2,12 +2,19 @@ package com.hcl.employeedirectory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EmployeeDirectoryApplication {
+public class EmployeeDirectoryApplication extends  SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(EmployeeDirectoryApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeDirectoryApplication.class, args);
 	}
+	
 
 }
